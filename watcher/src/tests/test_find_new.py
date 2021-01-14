a8e381233e410d6b51c2d11c98fde228a3a37a38
= <jlrine2@alaska.edu>
2021-01-14 10:26:13 -0900

    fix deployment related issues

--- watcher/src/tests/test_find_new.py
+++
@@ -1,8 +0,0 @@
-
-def test_get_rtc_granules():
-    subscription = {
-        'geometry': None,
-        'start': '20200101T00:00:00',
-        'end': '20200102T00:00:00',
-        'processing_types': ['SLC']
-    }
