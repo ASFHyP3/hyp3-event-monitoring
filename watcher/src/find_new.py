10ef4ed64f36352cefd822c55b836cbb7621f591
= <jlrine2@alaska.edu>
2021-01-15 13:44:50 -0900

    typo

--- watcher/src/find_new.py
+++ watcher/src/find_new.py
@@ -47,7 +47,7 @@ def submit_product_to_hyp3(subscription, granules):
                                    dem_matching=subscription['processing_parameters']['dem_matching'],
                                    include_dem=subscription['processing_parameters']['include_dem'],
                                    include_inc_map=subscription['processing_parameters']['include_inc_map'],
-                                   include_sattering_area=subscription['processing_parameters'][
+                                   include_scattering_area=subscription['processing_parameters'][
                                        'include_scattering_area'],
                                    radiometry=subscription['processing_parameters']['radiometry'],
                                    resolution=float(subscription['processing_parameters']['resolution']),
