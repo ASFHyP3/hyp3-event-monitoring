919c99321332317654842502b80ee38c54e1b30a
= <jlrine2@alaska.edu>
2021-01-29 12:15:49 -0900

    finish up draft of tests

--- harvest_products/src/harvest_products.py
+++ harvest_products/src/harvest_products.py
@@ -1,33 +1,67 @@
 from os import environ
 
 import boto3
+from boto3.dynamodb.conditions import Key
 
 from hyp3_sdk import HyP3
 
 DB = boto3.resource('dynamodb')
+S3 = boto3.resource('s3')
 
 
-def harvest(product):
-    pass
+def harvest(product, job):
+    destination_bucket = S3.Bucket(environ['BUCKET_NAME'])
+    copy_source = {
+        'Bucket': job.files[0]['s3']['bucket'],
+        'Key': job.files[0]['s3']['key'],
+    }
+    product_name = job.files[0]['filename']
+    destination_key = f'{product["event_id"]}/{product["product_id"]}/{product_name}'
+    destination_bucket.copy(copy_source, destination_key)
+
+    product = S3.Object(destination_bucket.name, destination_key)
+
+    return {
+        'browse_url': job.browse_images[-1],
+        'thumbnail_url': job.thumbnail_images[-1],
+        'product_name': product_name,
+        'product_size': product.content_length,
+        'product_url': f'https://{destination_bucket.name}.s3.amazonaws.com/{destination_key}'
+
+    }
 
 
 def is_succeeded(product):
-    return False
+    return product['status_code'] == "SUCCEEDED"
 
 
-def get_products():
+def update_product(product):
     table = DB.Table(environ['PRODUCT_TABLE'])
-    response = table.scan()
+    hyp3 = HyP3(environ['HYP3_URL'], username=environ['EDL_USERNAME'], password=environ['EDL_PASSWORD'])
+    job = hyp3._get_job_by_id(product['product_id'])
+    if job.complete():
+        if job.succeeded():
+            product['files'] = harvest(product, job)
+        product['status_code'] = job.status_code
+        table.put_item(Item=product)
+
+
+def get_incomplete_products():
+    table = DB.Table(environ['PRODUCT_TABLE'])
+    key_expression = Key('status_code').eq('PENDING')
+    response = table.query(IndexName='status_code', KeyConditionExpression=key_expression)
     products = response['Items']
     while 'LastEvaluatedKey' in response:
-        response = table.scan(
+        response = table.query(
+            IndexName='status_code',
+            KeyConditionExpression=key_expression,
             ExclusiveStartKey=response['LastEvaluatedKey'],
         )
         products.extend(response['Items'])
     return products
 
+
 def lambda_handler(event, context):
-    products = get_products()
+    products = get_incomplete_products()
     for product in products:
-        if is_succeeded(products):
-            harvest(product)
+        update_product(product)
--- tests/test_harvest_products.py
+++ tests/test_harvest_products.py
@@ -1,8 +1,14 @@
+import json
+from os import environ
 from uuid import uuid4
+
+import responses
+from hyp3_sdk.util import AUTH_URL
 
 import harvest_products
 
-def test_get_products(tables):
+
+def test_get_products(harvester_tables):
     mock_products = [
         {
             'event_id': '1',
@@ -22,13 +28,146 @@ def test_get_products(tables):
             'event_id': '3',
             'product_id': str(uuid4()),
             'granules': [],
-            'status_code': 'RUNNING',
+            'status_code': 'PENDING',
             'processing_date': '2020-01-01T00:00:00+00:00'
         },
     ]
     for product in mock_products:
-        tables.product_table.put_item(Item=product)
+        harvester_tables.product_table.put_item(Item=product)
 
-    products = harvest_products.get_products()
+    products = harvest_products.get_incomplete_products()
 
-    assert products == mock_products
+    assert products == mock_products[1:]
+
+
+@responses.activate
+def test_harvest(harvester_tables, s3_stubber):
+    product = {
+        'event_id': '1',
+        'product_id': 'foo',
+        'granules': [],
+        'status_code': 'PENDING',
+        'processing_date': '2020-01-01T00:00:00+00:00'
+    }
+
+    class MockJob():
+        files = [
+            {'filename': 'product.zip',
+             's3': {
+                 'bucket': 'BUCKET',
+                 'key': 'foo/product.zip',
+             },
+             },
+        ]
+        browse_images = [
+            'BROWSE_IMAGE_URL',
+        ]
+        thumbnail_images = [
+            'THUMBNAIL_IMAGE_URL',
+        ]
+
+    params = {
+        'Bucket': 'BUCKET',
+        'Key': 'foo/product.zip',
+    }
+    s3_response = {
+        'ContentLength': 123
+    }
+    s3_stubber.add_response(method='head_object', expected_params=params, service_response=s3_response)
+    params = {
+        'Bucket': environ['BUCKET_NAME'],
+        'Key': '1/foo/product.zip',
+        'CopySource': {
+            'Bucket': 'BUCKET',
+            'Key': 'foo/product.zip'
+        },
+    }
+    s3_response = {
+    }
+    s3_stubber.add_response(method='copy_object', expected_params=params, service_response=s3_response)
+    params = {
+        'Bucket': environ['BUCKET_NAME'],
+        'Key': '1/foo/product.zip',
+    }
+    s3_response = {
+        'ContentLength': 123
+    }
+    s3_stubber.add_response(method='head_object', expected_params=params, service_response=s3_response)
+
+    files = harvest_products.harvest(product, MockJob())
+
+    assert files == {
+        'browse_url': 'BROWSE_IMAGE_URL',
+        'thumbnail_url': 'THUMBNAIL_IMAGE_URL',
+        'product_name': 'product.zip',
+        'product_size': 123,
+        'product_url': f'https://{environ["BUCKET_NAME"]}.s3.amazonaws.com/1/foo/product.zip'
+    }
+
+
+def test_update_product(harvester_tables):
+    product = {
+        'event_id': '1',
+        'product_id': 'foo',
+        'granules': [],
+        'status_code': 'PENDING',
+        'processing_date': '2020-01-01T00:00:00+00:00'
+    }
+    responses.add(responses.GET, AUTH_URL)
+    hyp3_repsonse = {
+        'job_id': 'foo',
+        'job_type': 'RTC_GAMMA',
+        'name': 'event_id1',
+        'request_time': '2020-06-04T18:00:03+00:00',
+        'user_id': 'some_user',
+        'status_code': 'SUCCEEDED',
+        'browse_images': ['BROWSE_IMAGE_URL'],
+        'thumbnail_images': ['THUMBNAIL_IMAGE_URL'],
+        'files': [
+            {
+                's3': {
+                    'bucket': 'BUCKET',
+                    'key': 'foo/product.zip',
+                },
+            },
+        ],
+    }
+    responses.add(responses.GET, environ['HYP3_URL'] + '/jobs/foo', json.dumps(hyp3_repsonse))
+
+    def mock_harvest(foo, bar):
+        return {
+            'browse_url': 'BROWSE_IMAGE_URL',
+            'thumbnail_url': 'THUMBNAIL_IMAGE_URL',
+            'product_name': 'product.zip',
+            'product_size': 123,
+            'product_url': f'https://{environ["BUCKET_NAME"]}.s3.amazonaws.com/1/foo/product.zip'
+        }
+
+    harvest_products.harvest = mock_harvest
+
+    harvest_products.update_product(product)
+
+    updated_product = harvester_tables.product_table.scan()['Items'][0]
+
+    assert updated_product['status_code'] == 'SUCCEEDED'
+    assert updated_product['files'] == mock_harvest(None, None)
+
+
+def test_is_succeeded():
+    product = {
+        'event_id': '1',
+        'product_id': str(uuid4()),
+        'granules': [],
+        'status_code': 'SUCCEEDED',
+        'processing_date': '2020-01-01T00:00:00+00:00'
+    }
+    assert harvest_products.is_succeeded(product) is True
+
+    product = {
+        'event_id': '1',
+        'product_id': str(uuid4()),
+        'granules': [],
+        'status_code': 'PENDING',
+        'processing_date': '2020-01-01T00:00:00+00:00'
+    }
+    assert harvest_products.is_succeeded(product) is False
